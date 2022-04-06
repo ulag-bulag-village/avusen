daa25cf3c9fde7cf4ae22bed940bfc0ae6de9746
Ho Kim <ho.kim@ulagbulag.io>
2022-04-06 06:47:21 +0000

    Wrap codec

--- src/lib.rs
+++ src/lib.rs
@@ -8,16 +8,18 @@ pub mod result;
 pub mod source;
 pub mod value;
 
-pub fn encode<T>(val: &T) -> Result<Vec<u8>, rmp_serde::encode::Error>
-where
-    T: serde::Serialize + ?Sized,
-{
-    rmp_serde::encode::to_vec(val).map_err(Into::into)
-}
+pub mod codec {
+    pub fn encode<T>(val: &T) -> Result<Vec<u8>, rmp_serde::encode::Error>
+    where
+        T: serde::Serialize + ?Sized,
+    {
+        rmp_serde::encode::to_vec(val).map_err(Into::into)
+    }
 
-pub fn decode<T>(rd: &[u8]) -> Result<T, rmp_serde::decode::Error>
-where
-    T: serde::de::DeserializeOwned + ?Sized,
-{
-    rmp_serde::decode::from_read(rd).map_err(Into::into)
+    pub fn decode<T>(rd: &[u8]) -> Result<T, rmp_serde::decode::Error>
+    where
+        T: serde::de::DeserializeOwned + ?Sized,
+    {
+        rmp_serde::decode::from_read(rd).map_err(Into::into)
+    }
 }
